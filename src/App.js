f2d1b36cf1e0558742a9a3c6c632552d713c49e0
Olga <olga@Bogdans-MBP.fritz.box>
2021-09-01 13:17:55 +0200

    my commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react"
 import './App.css';
+import Todo from "./Todo";
+
+function getTodofromLocalStorage() {
+  let todosString = localStorage.getItem('todos')
+  if (todosString && todosString.length > 0) {
+    return todosString.split(",")
+  } else {
+    return []
+  }
+}
 
 function App() {
+  const [todos, setTodos] = useState(getTodofromLocalStorage()); //state todo
+  const [inputValue, setInputValue] = useState("");
+
+  function removeTodo(todo) {
+    setTodos(todos.filter((td) => (td !== todo)))
+  }
+
+  useEffect(() => {
+    localStorage.setItem('todos', todos)
+  }, [todos])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div id="app">
+      <h1 className="todos-title">To-do List</h1>
+      <div className="input-row">
+        <input
+          className="add-todo-input"
+          value={inputValue}
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
+        ></input>
+        <button
+          className="submit-button"
+          onClick={(e) => {
+            if (inputValue && inputValue.length > 0) { // fix bug of being able to add empty todo's
+              setTodos([...todos, inputValue]);
+              //clean up the field
+              setInputValue("");
+            }
+
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add Todo
+        </button>
+      </div>
+      <div className="todo-container">
+        {todos.map((todo) => (
+          <Todo todo={todo} removeTodo={removeTodo} />
+        ))}
+      </div>
+      <p>
+        You have <strong>{todos.length}</strong> tasks in progress
+      </p>
     </div>
   );
 }
